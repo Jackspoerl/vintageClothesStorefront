4de2e77042a1160dd52612cc4fb115e892318250
Nsun646 <nsun646@gmail.com>
2023-09-30 16:14:51 -0400

    java docs and edits

--- out/production/vintageClothesStorefront/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"Store","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"Store","l":"Store()","u":"%3Cinit%3E()"}];updateSearchResults();
